5c0b7a3d61a6f7cba6da7bcf7535ecdfeae7c290
Jumael Ahmed Khan <zumael@Jumaels-MacBook-Air.local>
2025-10-12 02:47:00 +0600

    first commit

---
+++ freecodecampDOM.js
@@ -0,0 +1,14 @@
+// const famContacts = document.getElementsByClassName("family");
+// console.log("this is the famcontacts in raw: ", famContacts);
+
+// let spreadFamContacts = [...famContacts];
+// spreadFamContacts.forEach(element => console.log(element))
+// console.log("above i am using spread operator and forEach");
+
+// const tagSelection = document.getElementsByTagName('p');
+// let spreadTagSelection = [...tagSelection]
+
+// console.log(spreadTagSelection)
+
+const selectTopic = document.querySelector("#topic");
+selectTopic.innerHTML = "<b>james</b> shaka laka boom"
